c042f24a39e992e2f01b8f72a99a86e4583d0d93
vamsi-purefire <vamsi@purefiretech.com>
2023-07-25 17:16:23 +0530

    Implement Auth Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import logo from './logo.svg';
 import './App.css';
 import { PowerBIEmbed } from 'powerbi-client-react';
 import { models } from 'powerbi-client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ReactDOM } from 'react';
 import Login from './Login';
-import { authContext, adalConfig } from './adalConfig'; // Create this file in the n
-
+import { runWithAdal } from 'react-adal';
+import { authContext, adalApiFetch, adalConfig } from './adalConfig'; // Create this file in the n
+import Appss from './AppHome';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
+  const login = () => {
+    authContext.login();
+  };
 
   useEffect(() => {
     authContext.handleWindowCallback();
@@ -30,20 +37,22 @@ const App = () => {
   const logout = () => {
     setIsAuthenticated(false);
     setAccessToken(" ");
+
   };
 
 
 
   return (
     <div className="App">
-     <button onClick={logout}>Logout</button> 
       {isAuthenticated ? (
+        <>
+        <button onClick={logout}>Logout</button> 
         <PowerBIEmbed
           embedConfig={{
             type: 'report',
             id: '0b899235-c7d8-4d83-a9c1-f20c47d95c05',
             embedUrl: 'https://app.powerbi.com/reportEmbed',
-            accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNWYzMzAwZGMtYTAwZS00Y2RkLTlhMmMtOTAwZDY5ZGVhYjUzLyIsImlhdCI6MTY5MDIwMjU5MSwibmJmIjoxNjkwMjAyNTkxLCJleHAiOjE2OTAyMDc5NDksImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVlFBcS84VEFBQUFHMVpBOGN2M21mbUdleGRZQkZjdkRlQ3JXRkhwdk9VU1Z0VG9pWGtnSWNpWE8vU0FNaHQ5cjlYT1Urd1l3NTZBV2sxLzUwYkJvSllNTk9GbWJpYUQ2U2lmTnE1aThzNnhlZWlUZjVYMEZiRT0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcGlkIjoiODcxYzAxMGYtNWU2MS00ZmIxLTgzYWMtOTg2MTBhN2U5MTEwIiwiYXBwaWRhY3IiOiIyIiwiZmFtaWx5X25hbWUiOiJTaGFybWEiLCJnaXZlbl9uYW1lIjoiUGl5dXNoIiwiaXBhZGRyIjoiMjQwMTo0OTAwOjFmMzI6OGZmZjo6MjdiOjQyMTQiLCJuYW1lIjoiUGl5dXNoIFNoYXJtYSIsIm9pZCI6ImZhOTI0YWNiLTBhMmEtNDE5NC05YmIzLTUyZmNhMDBhMzQ2NyIsInB1aWQiOiIxMDAzMjAwMjlGOTMxMEE1IiwicmgiOiIwLkFYRUEzQUF6WHc2ZzNVeWFMSkFOYWQ2clV3a0FBQUFBQUFBQXdBQUFBQUFBQUFDSEFDay4iLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzaWduaW5fc3RhdGUiOlsia21zaSJdLCJzdWIiOiJLTHlERklXNWVRVDl6VjBObHRiQjNOT2l2eVhIaUFDeFg0Q1FSSFpDZUhBIiwidGlkIjoiNWYzMzAwZGMtYTAwZS00Y2RkLTlhMmMtOTAwZDY5ZGVhYjUzIiwidW5pcXVlX25hbWUiOiJwaXl1c2hAcHVyZWZpcmV0ZWNoLmNvbSIsInVwbiI6InBpeXVzaEBwdXJlZmlyZXRlY2guY29tIiwidXRpIjoiN21DMVlXaEdCRUtORFB6Q2Y5VWxBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il19.cQj9GGzlgzTZEEhSgpE2mZBUojU4zhOeq1R3yXyhQuqB3PpCknB_Ktd2wVPkrAXwqfzzRTySm0ftT66VjZL_crP0It7g58I2QB3lYzeJHnmtzaXrwpGjb8CMVn4HJB0LwSg31XQAHoZ5A7LnA067U_9yfWxaGHwUftA7jHzz4wzM2l0ybHqw9tbTggde1dcuiuCgyrOBV13lw3YgkdA-lg3fw8roUHVmws_l_LcyNJHKnFoz1OSf8N_ZXN5z-DWm78FooZ8CW9zEtgLEk1L7R7FWYqCHW37x2lUvAGd_gLZBm2WK0xMZ98x73fUEeqfHpjiClDUgGzYvx6BqduC0ng',
+            accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNWYzMzAwZGMtYTAwZS00Y2RkLTlhMmMtOTAwZDY5ZGVhYjUzLyIsImlhdCI6MTY5MDI3ODI3NCwibmJmIjoxNjkwMjc4Mjc0LCJleHAiOjE2OTAyODM1MjUsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVlFBcS84VEFBQUE5VGd6TjRWM3Fjb0czMHRpdEQ5cEE1VmxzZmhPNmF5SzRBWDMrbFgzOFBXZnEyV3VzQTBpNHdFWGJKclMvT1hPY01OOVUvQjRsSldjdTAxSnpDL0MvUnJsRHYveldJbzJNdnAzek13ZGxVWT0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcGlkIjoiODcxYzAxMGYtNWU2MS00ZmIxLTgzYWMtOTg2MTBhN2U5MTEwIiwiYXBwaWRhY3IiOiIyIiwiZmFtaWx5X25hbWUiOiJTaGFybWEiLCJnaXZlbl9uYW1lIjoiUGl5dXNoIiwiaXBhZGRyIjoiMTIyLjE3My4yOS4yMjciLCJuYW1lIjoiUGl5dXNoIFNoYXJtYSIsIm9pZCI6ImZhOTI0YWNiLTBhMmEtNDE5NC05YmIzLTUyZmNhMDBhMzQ2NyIsInB1aWQiOiIxMDAzMjAwMjlGOTMxMEE1IiwicmgiOiIwLkFYRUEzQUF6WHc2ZzNVeWFMSkFOYWQ2clV3a0FBQUFBQUFBQXdBQUFBQUFBQUFDSEFDay4iLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzaWduaW5fc3RhdGUiOlsia21zaSJdLCJzdWIiOiJLTHlERklXNWVRVDl6VjBObHRiQjNOT2l2eVhIaUFDeFg0Q1FSSFpDZUhBIiwidGlkIjoiNWYzMzAwZGMtYTAwZS00Y2RkLTlhMmMtOTAwZDY5ZGVhYjUzIiwidW5pcXVlX25hbWUiOiJwaXl1c2hAcHVyZWZpcmV0ZWNoLmNvbSIsInVwbiI6InBpeXVzaEBwdXJlZmlyZXRlY2guY29tIiwidXRpIjoibzItcTNTbXp1MHExbkdOZDA0QXRBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il19.AQZ_l95gjxKgqSlIYTxFAGmafXgEgp225fROP--xu-8hpehL1MJvdBRGJHceXi5LimbbLfQw-deVs_yppLKOd73tFmE74wzhnufdzsUjpQgJyj8r9mvJ8hZKOhGNLsJKDpjDWwgH_gXj0YaNckmAj-Fh4BpkxZFPQjCc7FuKpIwvDxYKMjO8UN-oUvfcHvYGrVU7fNN6eBUxveXOAMN-7GjWmPkzRtbFhq1EZ9m0q0sEdrpGwSRka3SogacFpwjNAhTGihFApzK3U8b-DnbcIBHW65iVEZliAkvq1x7m8ZunvNfP3848CTCoYJeRcbQ6roHZsCQGK8k3qCT4uk2g4g',
             tokenType: models.TokenType.Aad,
             settings: {
               panes: {
@@ -75,8 +84,10 @@ const App = () => {
             console.log(embedObject);
             
           }}
-
+          
         />
+        </>
+        
       ) : (
         <Login />
       )}
--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,57 @@
 import React, { useEffect } from 'react';
 import { AuthenticationContext } from 'react-adal';
 import { authContext } from './adalConfig'; // Create this file in the n
-import "./login.css";
+//import "./login.css";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBInput
+}
+from 'mdb-react-ui-kit';
+
 const Login = () => {
   const handleLogin = () => {
     authContext.login();
   };
 
-  return (<div class="Main-cont">
-  <h1>Login to Access your reports</h1>
-  <p>Click the button below to log in with Microsoft:</p>
-  <button onClick={handleLogin}>Login with Microsoft</button>
-
+  return (
   
-  </div>
-    
+    <MDBContainer >
+
+    <MDBCard>
+      <MDBRow className='g-0'>
+
+        <MDBCol md='6'>
+          <MDBCardImage src='https://www.aaparx.com/hubfs/aapa_wide_hero_bg.jpg' alt="login form" className='rounded-start w-100'/>
+        </MDBCol>
+
+        <MDBCol md='6' className=''>
+          <MDBCardBody className='d-flex flex-column '>
+
+            <div className='d-flex flex-row justify-content-center align-items-center'>
+              <MDBCardImage src='https://www.aaparx.com/hs-fs/hubfs/aapa_logo.png?width=100&name=aapa_logo.png' />
+            </div>
+
+            <h5 className="fw-normal my-4 pb-3" style={{letterSpacing: '1px'}}>Sign into your account</h5>
+
+            <MDBBtn className="mb-4 w-100" size="lg" style={{backgroundColor: '#000'}} onClick={handleLogin}>
+              <MDBIcon fab icon="windows" className="mx-2"/>
+              Continue with Microsoft
+            </MDBBtn>
+
+          </MDBCardBody>
+        </MDBCol>
+
+      </MDBRow>
+    </MDBCard>
+
+  </MDBContainer>
   );
 };
 
